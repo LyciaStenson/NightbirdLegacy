0c7535e3cfcf22be2040137b189282fc59dd940c
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-07-18 22:06:48 +0100

    Still working on reading shader files

--- Nightbird/main.cpp
+++ Nightbird/main.cpp
@@ -24,7 +24,7 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Nightbird", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Nightbird", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -47,16 +47,14 @@ int main()
 		return -1;
 	}
 
-	// Successfully loaded OpenGL
 	std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
 
 	// Define the viewport dimensions
 	glViewport(0, 0, WIDTH, HEIGHT);
 
-	// BEGIN TRIANGLE
+	Shader thisShader("shader.vert", "shader.frag");
 
-	Shader thisShader("thisShader.vert", "thisShader.frag");
-
+	/*
 	float vertices[] = {
 		 0.5f,  0.5f, 0.0f, // top right
 		 0.5f, -0.5f, 0.0f, // bottom right
@@ -68,19 +66,25 @@ int main()
 		0, 1, 3,	// Triangle 1
 		1, 2, 3		// Triangle 2
 	};
+	*/
 
-	unsigned int VBO, VAO, EBO;
+	float vertices[] = {
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f};
+
+	unsigned int VBO, VAO; //, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	// glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), indecies, GL_STATIC_DRAW);
+	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), indecies, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
@@ -88,13 +92,14 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	// Game loop
+	// Render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//thisShader.use();
+		thisShader.use();
+
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -104,23 +109,20 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	// glDeleteBuffers(1, &EBO);
 
-	// Terminates GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
 	return 0;
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-	// make sure the viewport matches the new window dimensions; note that width and
-	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
 }
--- Nightbird/shader.cpp
+++ Nightbird/shader.cpp
@@ -8,7 +8,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 	std::ifstream fShaderFile;
 
 	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 	try
 	{
 		vShaderFile.open(vertexPath);
@@ -17,7 +17,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 		std::stringstream vShaderStream, fShaderStream;
 
 		vShaderStream << vShaderFile.rdbuf();
-		fShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
 
 		vShaderFile.close();
 		fShaderFile.close();
@@ -25,43 +25,34 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 		vertexCode = vShaderStream.str();
 		fragmentCode = fShaderStream.str();
 	}
-	catch(const std::exception& e)
+	catch(std::ifstream::failure& e)
 	{
-		std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+		std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
 	}
 
-	const char* vShaderCode = vertexCode.c_str();
-	const char* fShaderCode = fragmentCode.c_str();
+	//const char* vShaderCode = vertexCode.c_str();
+	//const char* fShaderCode = fragmentCode.c_str();
 
-	unsigned int vertex, fragment;
-	int success;
-	char infoLog[512];
+	//unsigned int vertex, fragment;
 
-	vertex = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex, 1, &vShaderCode, NULL);
-	glCompileShader(vertex);
-	checkCompileErrors(vertex, "VERTEX");
+	//vertex = glCreateShader(GL_VERTEX_SHADER);
+	//glShaderSource(vertex, 1, &vShaderCode, NULL);
+	//glCompileShader(vertex);
+	//checkCompileErrors(vertex, "VERTEX");
 
-	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-	if (success)
-	{
-		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+	//fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	//glShaderSource(fragment, 1, &fShaderCode, NULL);
+	//glCompileShader(fragment);
+	//checkCompileErrors(fragment, "FRAGMENT");
 
-	fragment = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment, 1, &fShaderCode, NULL);
-	glCompileShader(fragment);
-	checkCompileErrors(fragment, "FRAGMENT");
+	//ID = glCreateProgram();
+	//glAttachShader(ID, vertex);
+	//glAttachShader(ID, fragment);
+	//glLinkProgram(ID);
+	//checkCompileErrors(ID, "PROGRAM");
 
-	ID = glCreateProgram();
-	glAttachShader(ID, vertex);
-	glAttachShader(ID, fragment);
-	glLinkProgram(ID);
-	checkCompileErrors(ID, "PROGRAM");
-
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
+	//glDeleteShader(vertex);
+	//glDeleteShader(fragment);
 	
 }
 
@@ -104,7 +95,7 @@ void Shader::checkCompileErrors(unsigned int shader, std::string type)
 		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
 		if (!success)
 		{
-			glGetShaderInfoLog(shader, 2024, NULL, infoLog);
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
 			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << std::endl;
 		}
 	}
@@ -113,7 +104,7 @@ void Shader::checkCompileErrors(unsigned int shader, std::string type)
 		glGetProgramiv(shader, GL_LINK_STATUS, &success);
 		if (!success)
 		{
-			glGetProgramInfoLog(shader, 2024, NULL, infoLog);
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
 			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << std::endl;
 		}
 	}
