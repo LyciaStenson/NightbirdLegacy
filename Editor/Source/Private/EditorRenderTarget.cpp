2ecb2f9f94861d35c6a0aeda2a3b307cf623007b
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-10-16 23:00:29 +0100

    Editor custom font working

--- Editor/Source/Private/EditorRenderTarget.cpp
+++ Editor/Source/Private/EditorRenderTarget.cpp
@@ -16,8 +16,10 @@ EditorRenderTarget::EditorRenderTarget(GLFWwindow* window)
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
+	io.Fonts->AddFontFromFileTTF("Assets/Fonts/RobotoFlex-Regular.ttf", 16.0f);
+
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
+	ImGui_ImplOpenGL3_Init("#version 460");
 }
 
 EditorRenderTarget::~EditorRenderTarget()
