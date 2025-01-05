30e04af605b820d7a6d14c20b6f27d9cec8c3280
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-01-05 17:53:59 +0000

    Working on model loading

--- Engine/Source/Public/Components/MeshComponent.h
+++ Engine/Source/Public/Components/MeshComponent.h
@@ -59,12 +59,12 @@ struct MeshComponent
 	std::vector<Vertex> vertices;
 	std::vector<unsigned int> indices;
 	std::vector<Texture> textures;
-	
+
 	unsigned int texture = 0;
 	unsigned int VBO = 0;
 	unsigned int VAO = 0;
 	unsigned int EBO = 0;
-	
+
 	const char* vertexPath;
 	const char* fragmentPath;
 	const char* texturePath;
